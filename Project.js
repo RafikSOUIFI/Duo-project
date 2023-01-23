96d2bd13938ef800d867c92f6391941518143131
RafikSOUIFI <rafik.souifi@gmail.com>
2023-01-23 14:42:22 +0100

    2

---
+++ Project.js
@@ -0,0 +1,42 @@
+var plants=[
+{
+   name: "Snake plant",
+   source:"./img/Snake plant.jpg",
+   info: "The snake plant is a carefree, resilient houseplant that makes a bold, architectural statement in your home with tall, swordlike leaves and unique variegation."
+},
+{
+    name:"Four o'clock flower",
+    source:"./img/Four o'clock flower.jpg",
+    info: 
+ },
+ {
+    name:"Arabian jasmine",
+    source:"./img/Arabian jasmine.jpg",
+    info: "Jasminum sambac is a highly fragrant, evergreen shrub which produces clusters of white flowers that fade to pink. It blooms mainly in the summer - occasionally at other times too - and has a twining, scrambling habit. It has angular stems and glossy, waxy, dark green leaves."
+ },
+ {
+    name:"Bitter orange",
+    source:"./img/Bitter orange.jpg",
+    info: "Bitter orange, also known as Seville orange or sour orange, is a citrus fruit native to East Africa, the Arabian Peninsula, Syria, and Southeast Asia. This fruit is so sour that it's hard to eat by itself, but has been commonly used as the main ingredient in some marmalades."
+ },
+ {
+    name:"Garden tulip",
+    source:"./img/Garden tulip.jpg",
+    info: "The tulip produces two or three thick bluish green leaves that are clustered at the base of the plant. The usually solitary bell-shaped flowers have three petals and three sepals."
+ },
+ {
+    name:"Aloe vera",
+    source:"./img/Aloe vera.jpg",
+    info: "Aloe is a cactus-like plant that grows in hot, dry climates. It is cultivated in subtropical regions around the world, including the southern border areas of Texas, New Mexico, Arizona, and California. Historically, aloe has been used for skin conditions and was thought to improve baldness and promote wound healing."
+ },
+ {
+    name:"Marijuana",
+    source:"./img/Marijuana.jpg",
+    info: "Marijuana refers to the dried leaves, flowers, stems, and seeds from the Cannabis sativa or Cannabis indica plant. The plant contains the mind-altering chemical THC and other related compounds. People use marijuana by smoking, eating, drinking, or inhaling it."
+ },
+ {
+    name:"Indian shot",
+    source:"./img/Indian shot.jpg",
+    info: "Indian shot is a robust perennial herb up to 1m tall that grows from a thick, branching, underground rhizome. Leaves: Large green, broad leaves, which taper to a point. Flowers: Red or orange, usually yellow below, narrow with long petals."
+ }
+]
